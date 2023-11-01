3458dda9900e1937324ee2fefdf24379d97be891
PRASENJIT DAS <constprasenjit@PRASENJITs-MacBook-Air.local>
2023-11-01 07:21:14 +0530

    Weather watch project

---
+++ 11_weather_watch/src/components/SetUnits.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types"
+
+const SetUnits = ({ value, onSet }) => {
+    return <div className="set-units">
+        <label>UNITS</label>
+        <select value={value} onChange={onSet}>
+            <option value="C">Celsius</option>
+            <option value="F">Fahrenheit</option>
+        </select>
+    </div>;
+};
+
+SetUnits.prototype = {
+    value: PropTypes.string.isRequired,
+    onSet: PropTypes.func.isRequired
+}
+
+export default SetUnits;
